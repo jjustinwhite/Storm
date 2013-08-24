3a5775459a9700617651a24bc37544227ba7abb1
SirJustinWhite <Jjustinwhite@mac.com>
2013-08-24 15:23:57 -0500

    Button / Validations

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,6 @@ class Location < ActiveRecord::Base
 	attr_accessible :address, :latitude, :longitude
 	geocoded_by :address
   	after_validation :geocode
+
+  	validates :address, :presence => true
 end
