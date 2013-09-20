b9a5e59e2f0374874c7c7c747ae3afe1dd05ab57
SirJustinWhite <Jjustinwhite@mac.com>
2013-09-19 20:39:03 -0500

    UI Update + Daily Forecast

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -27,6 +27,33 @@ class LocationsController < ApplicationController
     @precip = 100 * @weather_json["currently"]["precipProbability"]
     @intensity = 100 * @weather_json["currently"]["precipIntensity"]
     @wind= @weather_json["currently"]["windSpeed"]
+
+    #Daily
+    @daily_summary_1 = get_summary_description(@weather_json["daily"]["data"][1]["summary"])
+    @daily_summary_2 = get_summary_description(@weather_json["daily"]["data"][2]["summary"])
+    @daily_summary_3 = get_summary_description(@weather_json["daily"]["data"][3]["summary"])
+    @daily_summary_4 = get_summary_description(@weather_json["daily"]["data"][4]["summary"])
+
+    @daily_icon_1 = get_weather_image(@weather_json["daily"]["data"][1]["icon"])
+    @daily_icon_2 = get_weather_image(@weather_json["daily"]["data"][2]["icon"])
+    @daily_icon_3 = get_weather_image(@weather_json["daily"]["data"][3]["icon"])
+    @daily_icon_4 = get_weather_image(@weather_json["daily"]["data"][4]["icon"])
+
+    @daily_lowtemp_1 = get_temperature_info(@weather_json["daily"]["data"][1]["temperatureMin"])
+    @daily_lowtemp_2 = get_temperature_info(@weather_json["daily"]["data"][2]["temperatureMin"])
+    @daily_lowtemp_3 = get_temperature_info(@weather_json["daily"]["data"][3]["temperatureMin"])
+    @daily_lowtemp_4 = get_temperature_info(@weather_json["daily"]["data"][4]["temperatureMin"])
+
+    @daily_hightemp_1 = get_temperature_info(@weather_json["daily"]["data"][1]["temperatureMax"])
+    @daily_hightemp_2 = get_temperature_info(@weather_json["daily"]["data"][2]["temperatureMax"])
+    @daily_hightemp_3 = get_temperature_info(@weather_json["daily"]["data"][3]["temperatureMax"])
+    @daily_hightemp_4 = get_temperature_info(@weather_json["daily"]["data"][4]["temperatureMax"])
+
+    #Hourly
+    @hourly_summary = @weather_json["hourly"]["summary"]
+
+    #Minutely
+    @minutely_summary = @weather_json["minutely"]["summary"]
   end
 
   # GET /locations/new
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def navMainAktiv(actionName)
+    if params[:controller] == actionName    
+    "active"
+    end
+	end
 end
